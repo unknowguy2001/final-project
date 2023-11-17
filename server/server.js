06497b99ac9d3f9f625b3cd2f07db85e2cd8384e
Unknowguy <unknowguy2001@gmail.com>
2023-11-17 20:30:24 +0700

    chore(server): setup server side

---
+++ server/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`app running at port: ${port} `);
+});
