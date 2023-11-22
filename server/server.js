823083f015473038d53d9c07cbd350d7b6f43308
Unknowguy <unknowguy2001@gmail.com>
2023-11-22 16:42:54 +0700

    chore : add option to port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cors());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`app running at port: ${port} `);
