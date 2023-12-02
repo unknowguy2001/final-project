64d9e3830bdde0d42bdccffa929282b1cd7a7a3e
pufferbommy <pufferbommy@gmail.com>
2023-12-02 22:31:59 +0700

    feat: add swagger docs

--- final-project-server/server.js
+++ final-project-server/server.js
@@ -2,6 +2,9 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDocument = require("./swagger.json");
 
 const authRouter = require("./routes/auth");
 const companiesRouter = require("./routes/companies");
@@ -17,9 +20,9 @@ app.use(
   })
 );
 app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
-
 app.use("/auth", authRouter);
 app.use("/companies", companiesRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT;
 app.listen(port, () => {
