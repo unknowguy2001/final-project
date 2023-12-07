8750e0660f752c14c700420df7159dd6e16256ff
pufferbommy <pufferbommy@gmail.com>
2023-12-07 14:58:23 +0700

    fix: handle jwt expired

--- final-project-server/utils/token.js
+++ final-project-server/utils/token.js
@@ -1,12 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (token, tokenType) => {
-  const secret =
-    tokenType === "access"
-      ? process.env.JWT_ACCESS_TOKEN_SECRET
-      : process.env.JWT_REFRESH_TOKEN_SECRET;
-  const payload = jwt.verify(token, secret);
-  return payload;
+  try {
+    const secret =
+      tokenType === "access"
+        ? process.env.JWT_ACCESS_TOKEN_SECRET
+        : process.env.JWT_REFRESH_TOKEN_SECRET;
+    const payload = jwt.verify(token, secret);
+    return payload;
+  } catch (error) {
+    return null;
+  }
 };
 
 const generateToken = (payload, tokenType) => {
