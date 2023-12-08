5ead8d374c548c5280a74c004ae5246a3595b493
pufferbommy <pufferbommy@gmail.com>
2023-12-08 19:57:19 +0700

    feat: smoother cursor in company card

--- final-project-client/src/components/company-card/useFunctions.tsx
+++ final-project-client/src/components/company-card/useFunctions.tsx
@@ -16,13 +16,18 @@ export const useFunctions = ({ company }: UseFunctionsProps) => {
     navigate(`/companies/${company.id}`);
   };
 
-  const handleMouseEnter = () => {
+  const handleMouseEnter = (e: MouseEvent) => {
     if (floatingCircleRef.current) {
       floatingCircleRef.current!.style.removeProperty("left");
       floatingCircleRef.current!.style.removeProperty("top");
-      floatingCircleRef.current.style.opacity = "1";
       floatingCircleRef.current.style.transform =
         "scale(1.25) translate(-50%, -50%)";
+      const { x, y } = cardRef.current!.getBoundingClientRect();
+      if (floatingCircleRef.current) {
+        floatingCircleRef.current.style.opacity = "1";
+        floatingCircleRef.current.style.left = `${e.clientX - x}px`;
+        floatingCircleRef.current.style.top = `${e.clientY - y}px`;
+      }
     }
   };
 
