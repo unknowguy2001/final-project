83d45058d1410da0bc04b9c78ceeb059c7d55486
pufferbommy <pufferbommy@gmail.com>
2023-12-31 14:21:28 +0700

    feat: add new origin frontend url

--- final-project-server/server.js
+++ final-project-server/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://intern.comsci-rmutp.com"],
     credentials: true,
   })
 );
