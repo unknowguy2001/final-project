b875bd3cc7042a7d521494703472be4d10467dda
pufferbommy <pufferbommy@gmail.com>
2024-01-22 18:03:26 +0700

    fix: wrong import

--- final-project-server/api/auth/auth.handlers.js
+++ final-project-server/api/auth/auth.handlers.js
@@ -1,7 +1,7 @@
 const argon2 = require("argon2");
 
 const { prisma } = require("../../utils/prisma");
-const { verifyToken, generateToken } = require("./auth.helpers");
+const { verifyToken, generateToken } = require("../../utils/token");
 
 module.exports.login = async (req, res) => {
   const { username, password } = req.body;
