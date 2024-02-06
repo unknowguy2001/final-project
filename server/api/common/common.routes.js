edf1e239e2074aceb5024c7896ba06370ac7e110
pufferbommy <pufferbommy@gmail.com>
2024-02-06 23:08:42 +0700

    fix: access role for common api routes

--- server/api/common/common.routes.js
+++ server/api/common/common.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const commonHandlers = require("./common.handlers");
+const { authorization } = require("../../middleware/authorization");
 
 const commonRoutes = express.Router();
 
-commonRoutes.get("/roles", commonHandlers.roles);
+commonRoutes.get("/roles", authorization(2), commonHandlers.roles);
 commonRoutes.get("/hashtags", commonHandlers.hashtags);
 
 module.exports = commonRoutes;
