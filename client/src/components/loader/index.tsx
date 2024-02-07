13b0b2023ffc3ca0e1c527f2d5724d7a0531d9ad
pufferbommy <pufferbommy@gmail.com>
2024-02-07 13:15:47 +0700

    fix: full screen height loader

--- client/src/components/loader/index.tsx
+++ client/src/components/loader/index.tsx
@@ -3,7 +3,7 @@ import DotLoader from "react-spinners/DotLoader";
 
 export const Loader = () => {
   return (
-    <Flex height="100%" justifyContent="center" alignItems="center">
+    <Flex height="100dvh" justifyContent="center" alignItems="center">
       <DotLoader color="#6E2996" />
     </Flex>
   );
