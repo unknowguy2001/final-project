0238ba8a61cc9f319debbf1c53b6f5b54bac4374
pufferbommy <pufferbommy@gmail.com>
2024-02-12 10:24:47 +0700

    fix: delete million

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,9 @@
-import million from "million/compiler";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
